fc04a293d12163ac0bb420ac1469822dc9fdd5d9
dedektegarapriyandi <dedektegarapriyandi@gmail.com>
2021-09-25 22:15:06 +0700

    create function for slider bg

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,8 @@
 const randomBtn = document.querySelector(".random-btn");
+const slider = document.querySelectorAll(".slider-range");
+
 const displayBg = document.querySelector(".display-bg");
+
 
 randomColor = () => {
     let r = Math.round(Math.random() * 255);
@@ -10,4 +13,18 @@ randomColor = () => {
     console.log(`rgba(${r}, ${g}, ${b})`)
 }
 
+sliderColor = (e) => {
+    for(i = 0; i < slider.length; i++) {
+        slider[i].addEventListener("input", () => {
+            let r,g,b;
+            r = slider[0].value;
+            g = slider[1].value;
+            b = slider[2].value;
+
+            displayBg.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        })
+    }
+}
+
 randomBtn.addEventListener("click", randomColor);
+sliderColor();
